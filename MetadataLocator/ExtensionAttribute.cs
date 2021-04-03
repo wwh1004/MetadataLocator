7949f67e26babf24c098efbdbdd06d22635433a4
wwh1004 <wwh1004@hotmail.com>
2021-04-03 23:38:00 +0800

    cleanup

--- MetadataLocator/ExtensionAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace System.Runtime.CompilerServices {
-	[AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class | AttributeTargets.Method)]
-	internal sealed class ExtensionAttribute : Attribute {
-		public ExtensionAttribute() {
-		}
-	}
-}
